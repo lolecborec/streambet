3fa2c9e5068f87d952912d4f08a1568246ae9a93
lolecborec <tomaswagner77@email.cz>
2025-05-20 17:00:00 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-let events = [];
-
-function calculatePercentages(bets) {
-  const total = bets.reduce((a, b) => a + b, 0);
-  return bets.map(b => total > 0 ? (b / total) * 100 : 0);
-}
-
-// (zkráceno kvůli délce)
